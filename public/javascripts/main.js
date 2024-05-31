ef2ce4d0a2dd4d54c1f6a36c1f3c597ffde19876
NguyenCong04 <nguyentatcong2004@gmail.com>
2024-05-31 17:36:40 +0700

    tối ưu website

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,122 +1,12 @@
-let filesSelected = false;
-document.getElementById('upload').addEventListener('change', async function (event) {
-    const formData = new FormData();
-    const files = document.getElementById('file').files;
-    const resultContainer = document.getElementById("result-container-1");
-    const loading = document.getElementById("load");
-    loading.style.display = "block";
-    resultContainer.style.opacity = 0.5
-    const maxFileSize = 1024 * 1024; // 3MB
-    const errorMessage = document.getElementById('error-message');
-
-    errorMessage.style.display = "none";
-    // Hide result container by default
-    resultContainer.style.display = "none";
-
-    if (files) {
-        for (let i = 0; i < files.length; i++) {
-            if (files[i].size > maxFileSize || files.length > 1) {
-                errorMessage.style.display = "block";
-                setTimeout(function () {
-                    errorMessage.style.display = "none";
-                }, 4000); // Hide after 3 seconds
-                loading.style.display = "none";
-                filesSelected = false; // Set filesSelected to false when encountering an error
-                return;
-            }
-        }
-        errorMessage.style.display = "none";
-
-        // Set filesSelected to true if at least one file is selected
-        filesSelected = files.length > 0;
-    }
-
-    // Render result container only if files are selected
-    if (filesSelected) {
-        resultContainer.style.display = "flex";
-    } else {
-        resultContainer.style.display = "none"; // Hide result container if no files are selected
-    }
-
-    for (let i = 0; i < files.length; i++) {
-        formData.append('image', files[i]);
-    }
-
-    try {
-        if (files.length !== 0) {
-            const response = await fetch('upload', {
-                method: 'POST',
-                body: formData
-            });
-            const data = await response.json();
-            const downloadLinks = data.compressedImages.map(image => `
+let filesSelected=!1;function formatFileSize(e){return e<1024?e+" Bytes":e<1048576?(e/1024).toFixed(1)+" KB":(e/1048576).toFixed(1)+" MB"}document.getElementById("upload").addEventListener("change",async function(e){let l=new FormData,t=document.getElementById("file").files,s=document.getElementById("result-container-1"),a=document.getElementById("load");a.style.display="block",s.style.opacity=.5;let n=document.getElementById("error-message");if(n.style.display="none",s.style.display="none",t){for(let o=0;o<t.length;o++)if(t[o].size>1048576||t.length>1){n.style.display="block",setTimeout(function(){n.style.display="none"},4e3),a.style.display="none",filesSelected=!1;return}n.style.display="none",filesSelected=t.length>0}filesSelected?s.style.display="flex":s.style.display="none";for(let c=0;c<t.length;c++)l.append("image",t[c]);try{if(0!==t.length){let d=await fetch("upload",{method:"POST",body:l}),y=await d.json(),r=y.compressedImages.map(e=>`
                  <div style="" class="custom-details">
                      <div class="" style="display: flex; flex-wrap: wrap">
-                         <p class="customP"><b>Name:</b> ${image.name}</p>
-                         <p class="customP" style="color: red"><b style="color: black">Size(Befor): </b>${formatFileSize(image.originalSize)}</p>
+                         <p class="customP"><b>Name:</b> ${e.name}</p>
+                         <p class="customP" style="color: red"><b style="color: black">Size(Befor): </b>${formatFileSize(e.originalSize)}</p>
                      </div>
                      <div class=""style="display: flex;flex-wrap: wrap">
-                         <p class="customP" style="color: #7eb631"><b style="color: black">Size(After): </b>${formatFileSize(image.compSize)}</p>
-                         <a class="a-download" href="data:image/${image.format};base64,${image.buffer}" download="compress_${image.name}">Download</a>
+                         <p class="customP" style="color: #7eb631"><b style="color: black">Size(After): </b>${formatFileSize(e.compSize)}</p>
+                         <a class="a-download" href="data:image/${e.format};base64,${e.buffer}" download="compress_${e.name}">Download</a>
                      </div>
                  </div>
-             `);
-            document.getElementById('result-container').innerHTML = downloadLinks.join('');
-        }
-
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        loading.style.display = "none";
-        resultContainer.style.opacity = 1
-    }
-});
-
-
-function formatFileSize(bytes) {
-    if (bytes < 1024) return bytes + ' Bytes';
-    else if (bytes < 1048576) return (bytes / 1024).toFixed(1) + ' KB';
-    else return (bytes / 1048576).toFixed(1) + ' MB';
-}
-
-
-//Config nav
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-//Sử lý UI FQAs
-const acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        const panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
-
-    toggle.addEventListener('click', () => {
-        // Add show-menu class to nav menu
-        nav.classList.toggle('show-menu')
-
-        // Add show-icon to show and hide the menu icon
-        toggle.classList.toggle('show-icon')
-    })
-}
-
-showMenu('nav-toggle', 'nav-menu')
+             `);document.getElementById("result-container").innerHTML=r.join("")}}catch(m){console.error("Error:",m)}finally{a.style.display="none",s.style.opacity=1}});const acc=document.getElementsByClassName("accordion");let i;for(i=0;i<acc.length;i++)acc[i].addEventListener("click",function(){this.classList.toggle("active");let e=this.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"});const showMenu=(e,l)=>{let t=document.getElementById(e),s=document.getElementById(l);t.addEventListener("click",()=>{s.classList.toggle("show-menu"),t.classList.toggle("show-icon")})};showMenu("nav-toggle","nav-menu");
